c6ac19c7d1c109b7edaa9b27bd400ccc84c488a6
Jayla (they/them) <jakeclegg99@gmail.com>
2024-12-04 11:27:59 +0000

    Created FoodAdapter Class implementing  INuitrition

---
+++ cs-design-patterns-sprint/Food/NutritionalCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_design_patterns_sprint
+{
+    public class NutritionalCalculator : INutritionalCalculator
+    {
+
+        private Food Food { get; set; }
+        private string name { get; set; }
+        private double calories { get; set; }
+        public NutritionalCalculator(Food food)
+        {
+
+            name = food.Name;
+            calories = food.Calories;
+
+        }
+
+        public double CalculateProtein()
+        {
+
+            double proteinConversionFactor = 0.1;
+            double baseProteinContent = 5.0;
+            double protein = calories / 100.0 * proteinConversionFactor + baseProteinContent;
+
+
+            return protein;
+
+        }
+        public Vitamins CalculateVitamins()
+        {
+            
+            int vitaminA = (int)(calories / 10);
+            int vitaminC = (int)(calories / 20);
+            Vitamins returnVit = new Vitamins(vitaminA, vitaminC);
+            return returnVit;
+        }
+    }
+}
--- cs-design-patterns-sprint/Program.cs
+++ cs-design-patterns-sprint/Program.cs
@@ -5,32 +5,32 @@ using cs_design_patterns_sprint;
 var config = new ConfigManager("https://example.com/db/url", 1000, 2000);
 
 // PART ONE
-Person person1 = new Person.Builder("Juniper", "Doolittle", 30).WithEmail("juniper@example.com").WithAddress("12 High Street").Build();
-Console.WriteLine(person1);
+//Person person1 = new Person.Builder("Juniper", "Doolittle", 30).WithEmail("juniper@example.com").WithAddress("12 High Street").Build();
+//Console.WriteLine(person1);
 config.IncrementDataCount(); // ignore this for now
 
 // PART TWO
-//List<Food> foods = new ([
-//    new Food("Orange", 60),
-//    new Food("Beef",  400),
-//    new Food ("A stick", 3 ),
-//    new Food("Pasta", 200)
-//]);
+List<Food> foods = new([
+    new Food("Orange", 60),
+    new Food("Beef",  400),
+    new Food ("A stick", 3 ),
+    new Food("Pasta", 200)
+]);
 
-//foreach (var food in foods)
-//{
-//    NutritionalCalculator nutritionalCalculator = new TODO(food); // TODO
+foreach (var food in foods)
+{
+    NutritionalCalculator nutritionalCalculator = new NutritionalCalculator(food); // TODO
 
-//    double protein = nutritionalCalculator.CalculateProtein();
-//    Vitamins vitamins = nutritionalCalculator.CalculateVitamins();
+    double protein = nutritionalCalculator.CalculateProtein();
+    Vitamins vitamins = nutritionalCalculator.CalculateVitamins();
 
-//    Console.WriteLine("Protein in " + food.Name + ": " + protein + " grams");
-//    Console.WriteLine("Vitamins in " + food.Name + ": Vitamin A=" + vitamins.VitaminA);
-//}
+    Console.WriteLine("Protein in " + food.Name + ": " + protein + " grams");
+   Console.WriteLine("Vitamins in " + food.Name + ": Vitamin A=" + vitamins.VitaminA);
+}
 config.IncrementDataCount(); // ignore this for now
 
 // PART THREE
-// ImaginaryDatabase.FlushDataQueue();
+ ImaginaryDatabase.FlushDataQueue();
 
 // PART FOUR
 // ITransport basicTransport = new BasicTransport("Basic");
